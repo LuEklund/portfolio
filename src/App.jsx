dbe80803d7081505f6f81ef11d6cc2e8d012b469
LuEklund <luckeeklund@gmail.com>
2023-06-27 10:20:28 +0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import Contact from "../../portfolio/src/componets/Contact"
+import Hero from "../../portfolio/src/componets/Hero"
+import Who from "../../portfolio/src/componets/Who"
+import Works from "../../portfolio/src/componets/Works"
+import styled from "styled-components"
+
+const Container = styled.div`
+  height: 100vh;
+  scroll-snap-type: y mandatory;
+  scroll-behavior: smooth;
+  overflow-y: auto;
+  scrollbar-width: none;
+  color: white;
+  background: url("./img/bg.jpeg");
+  &::-webkit-scrollbar{
+    display:none;
+  }
+  `;
+
+function App() {
+
+  return (
+    <Container>
+      <Hero/>
+      <Who/>
+      <Works/>
+      <Contact/>
+    </Container>
+  )
+}
+
+export default App
---
+++ src/componets/Navbar.jsx
@@ -0,0 +1,44 @@
+import React from 'react'
+import styled from 'styled-components'
+
+
+const Section = styled.div`
+    display: flex;
+    justify-content: center;
+  `;
+
+const Container = styled.div`
+    width: 140px;
+    background-color: rebeccapurple;
+  `;
+const Links = styled.div``;
+const Logo = styled.img``;
+const List = styled.ul``;
+const ListItem = styled.li``;
+const Icon = styled.img``;
+const Icons = styled.div``;
+const Button = styled.button``;
+
+const Navbar = () => {
+  return (
+    <Section>
+        <Container>
+            <Links>
+            <Logo src="./img/logo.png"/>
+            <List>
+                <ListItem>Home</ListItem>
+                <ListItem>Studio</ListItem>
+                <ListItem>Works</ListItem>
+                <ListItem>Contact</ListItem>
+            </List>
+            </Links>
+            <Icons>
+                <Icon src="./img/search.png"/>
+                <button>Hire Now</button>
+            </Icons>
+        </Container>
+    </Section>
+  )
+}
+
+export default Navbar
